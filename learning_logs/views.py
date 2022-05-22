50e4dcaba74b65cdd30c2c35443acc8bbe1e5fc4
Yerdos Sadvakassov <erdos.sadvakasov@gmail.com>
2022-05-22 15:25:13 +0600

    get_object_or_404 added

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm,EntryForm
@@ -20,7 +20,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     #Проверка что тема принадлежит текущему пользователю
     check_topic_owner(topic, request)
 
